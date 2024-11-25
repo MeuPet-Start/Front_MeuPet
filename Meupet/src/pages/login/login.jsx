739883538ba5dc4105eb311b229f3ee216e89770
Natan Lima <nfsl@cin.ufpe.br>
2024-11-24 23:33:10 -0300

    Integração com Back

--- Meupet/src/pages/login/login.jsx
+++ Meupet/src/pages/login/login.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 import {
   LoginContainer,
   LoginCard,
@@ -11,7 +13,6 @@ import {
   Input,
   InputContainer,
   Button,
-  Divider,
   LinkText,
   Background,
 } from "./loginStyle";
@@ -21,9 +22,42 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("E-mail inválido")
+        .required("O e-mail é obrigatório"),
+      password: Yup.string()
+        .min(6, "A senha deve ter pelo menos 6 caracteres")
+        .required("A senha é obrigatória"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post("http://localhost:8080/api/login", values);
+
+        if (response.status === 200) {
+          alert("Login bem-sucedido!");
+          navigate("/home"); 
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          alert("Credenciais inválidas. Tente novamente.");
+        } else {
+          console.error("Erro no login:", error);
+          alert("Ocorreu um erro inesperado. Tente novamente mais tarde.");
+        }
+      }
+    },
+  });
 
   return (
     <Background>
@@ -34,24 +68,42 @@ const Login = () => {
             <LogoText>Meu PET</LogoText>
           </LogoContainer>
           <Title>LOGIN</Title>
-          <Input type="email" placeholder="E-mail" />
-          <InputContainer>
+          <form onSubmit={formik.handleSubmit}>
             <Input
-              type={showPassword ? "text" : "password"}
-              placeholder="Senha"
+              type="email"
+              name="email"
+              placeholder="E-mail*"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
-            <span onClick={togglePasswordVisibility}>
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </span>
-          </InputContainer>
-          <LinkText href="/cadastro">Não tem uma conta? Cadastre-se</LinkText>
-          <Button>Entrar com gov.br</Button>
-          <Button>Entrar com Conecta</Button>
-          <Divider />
-          <Button secondary onClick={() => navigate("/")}>
-            Voltar
-          </Button>
-          <Button>Entrar</Button>
+            {formik.touched.email && formik.errors.email ? (
+              <div style={{ color: "red" }}>{formik.errors.email}</div>
+            ) : null}
+
+            <InputContainer>
+              <Input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Senha"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              <span onClick={togglePasswordVisibility}>
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </InputContainer>
+            {formik.touched.password && formik.errors.password ? (
+              <div style={{ color: "red" }}>{formik.errors.password}</div>
+            ) : null}
+
+            <LinkText href="/cadastro">Não tem uma conta? Cadastre-se</LinkText>
+            <Button secondary onClick={() => navigate("/")}>
+              Voltar
+            </Button>
+            <Button type="submit">Entrar</Button>
+          </form>
         </LoginCard>
       </LoginContainer>
     </Background>
