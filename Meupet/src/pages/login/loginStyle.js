d20c81f7dda631b764f250f1a8c570437afcc67d
Natan Lima <nfsl@cin.ufpe.br>
2024-11-25 23:39:04 -0300

    Integração e Estilização Cadastro e Login

--- Meupet/src/pages/login/loginStyle.js
+++ Meupet/src/pages/login/loginStyle.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import backgroundImage from '../../assets/background_login.png'; 
+import styled from "styled-components";
+import backgroundImage from "../../assets/background_login.png";
 
 export const Background = styled.div`
   background-image: url(${backgroundImage});
@@ -28,13 +28,13 @@ export const LogoContainer = styled.div`
 `;
 
 export const LogoImage = styled.img`
-  width: 80px;
-  height: 80px;
-  margin-right: 0.75rem;
+  width: 90px;
+  height: 90px;
+  margin-right: 0.25rem;
 `;
 
 export const LogoText = styled.h1`
-  color: #00316a;
+  color: #2a4f6a;
   font-size: 2rem;
 `;
 
@@ -46,19 +46,20 @@ export const LoginCard = styled.div`
   text-align: center;
   width: 100%;
   max-width: 400px;
-  border: 3px solid #00316a;
+  border: 2px solid #2a4f6a;
 `;
 
 export const Title = styled.h2`
   font-size: 2rem;
-  color: #00316a;
+  font-weight: 500;
+  color: #0;
   margin-bottom: 1rem;
+  font-family: "Poppins", sans-serif;
 `;
 
 export const InputContainer = styled.div`
   position: relative;
   width: 100%;
-  margin-bottom: 1rem;
 
   span {
     position: absolute;
@@ -70,7 +71,7 @@ export const InputContainer = styled.div`
     font-size: 1.2rem;
 
     &:hover {
-      color: #00316a;
+      color: #2a4f6a;
     }
   }
 `;
@@ -78,26 +79,35 @@ export const InputContainer = styled.div`
 export const Input = styled.input`
   width: 100%;
   padding: 0.75rem 2.5rem 0.75rem 0.75rem;
-  border: 1px solid #ccc;
+  border: 1px solid #2a4f6a;
   border-radius: 5px;
   font-size: 1rem;
   margin-bottom: 1rem;
 `;
 
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 0.75rem;
+  margin-top: 1rem;
+`;
+
 export const Button = styled.button`
-  width: 100%;
+  flex: 1; 
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
   padding: 0.75rem;
-  margin: 0.5rem 0;
   font-size: 1rem;
   border: none;
   border-radius: 5px;
   cursor: pointer;
-  background-color: ${(props) => (props.secondary ? 'white' : '#00316a')};
-  color: ${(props) => (props.secondary ? '#00316a' : 'white')};
-  border: ${(props) => (props.secondary ? '1px solid #00316a' : 'none')};
+  background-color: ${(props) => (props.secondary ? "white" : "#2a4f6a")};
+  color: ${(props) => (props.secondary ? "#2a4f6a" : "white")};
+  border: ${(props) => (props.secondary ? "1px solid #2a4f6a" : "none")};
+  transition: background-color 0.3s;
 
   &:hover {
-    background-color: ${(props) => (props.secondary ? '#f0f0f0' : '#002554')};
+    background-color: ${(props) => (props.secondary ? "#f0f0f0" : "#002554")};
   }
 `;
 
@@ -109,8 +119,42 @@ export const Divider = styled.hr`
 
 export const LinkText = styled.a`
   font-size: 0.9rem;
-  color: #00316a;
+  font-family: "Poppins", sans-serif;
+  color: #2a4f6a;
   text-decoration: none;
+  text-align: right;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const ErrorText = styled.div`
+  color: red;
+  font-size: 0.8rem;
+  font-family: "Poppins", sans-serif;
+  text-align: left;
+  margin-top: -0.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const Label = styled.label`
+  font-size: 0.9rem;
+  font-family: "Poppins", sans-serif;
+  color: #2a4f6a; /* Azul escuro */
+  margin-bottom: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LabelLink = styled.a`
+  font-size: 0.8rem;
+  color: #2a4f6a;
+  text-decoration: none;
+  margin-left: 0.5rem;
+  font-family: "Poppins", sans-serif;
+  text-align: right;
 
   &:hover {
     text-decoration: underline;
--- Meupet/src/pages/registro/register.jsx
+++ Meupet/src/pages/registro/register.jsx
@@ -1,108 +1,421 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { 
-  RegisterContainer, 
-  RegisterCard, 
-  Logo, 
-  Title, 
-  InputContainer, 
-  Input, 
-  Button, 
-  LinkText, 
-  Background 
-} from './registerStyle';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import logoImage from '../../assets/logo.png';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import {
+  RegisterContainer,
+  RegisterCard,
+  Logo,
+  Title,
+  InputContainer,
+  Input,
+  Button,
+  LinkText,
+  Background,
+  ErrorText,
+  ButtonContainer,
+  ButtonEscolhaContainer,
+  ButtonEscolha,
+  Label,
+} from "./registerStyle";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import logoImage from "../../assets/logo.png";
 
 const Register = () => {
   const navigate = useNavigate();
+  const [currentStep, setCurrentStep] = useState(0);
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [cpfCnpj, setCpfCnpj] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
 
-  const handleCpfCnpjChange = (e) => {
-    const value = e.target.value.replace(/\D/g, '');
+  const stepValidationSchemas = [
+    Yup.object().shape({
+      userType: Yup.string().required("Escolha uma opção."),
+    }),
+    Yup.object().shape({
+      cpfCnpj: Yup.string()
+        .required("O CPF ou CNPJ é obrigatório.")
+        .min(11, "O CPF/CNPJ deve ter pelo menos 11 caracteres.")
+        .max(18, "O CPF/CNPJ deve ter no máximo 18 caracteres."),
+      name: Yup.string().required("O nome é obrigatório."),
+      socialName: Yup.string(),
+      birthDate: Yup.string().required("A data de nascimento é obrigatória."),
+      email: Yup.string()
+        .email("Insira um e-mail válido.")
+        .required("O e-mail é obrigatório."),
+      phone: Yup.string()
+        .min(10, "O telefone deve ter pelo menos 10 dígitos.")
+        .max(15, "O telefone deve ter no máximo 15 dígitos.")
+        .required("O telefone é obrigatório."),
+    }),
+    Yup.object().shape({
+      email: Yup.string()
+        .email("Insira um e-mail válido.")
+        .required("O e-mail é obrigatório."),
+      phone: Yup.string()
+        .min(10, "O telefone deve ter pelo menos 10 dígitos.")
+        .max(15, "O telefone deve ter no máximo 15 dígitos.")
+        .required("O telefone é obrigatório."),
+      password: Yup.string()
+        .min(6, "A senha deve ter pelo menos 6 caracteres")
+        .required("A senha é obrigatória."),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "As senhas devem ser iguais")
+        .required("A confirmação da senha é obrigatória."),
+    }),
+  ];
 
-    const formatted =
-      value.length <= 11
-        ? value
-            .replace(/^(\d{3})(\d)/, '$1.$2') 
-            .replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3') 
-            .replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4') 
-        : value
-            .replace(/^(\d{2})(\d)/, '$1.$2') 
-            .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3') 
-            .replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3/$4') 
-            .replace(/^(\d{2})\.(\d{3})\.(\d{3})\/(\d{4})(\d)/, '$1.$2.$3/$4-$5'); 
+  const formik = useFormik({
+    initialValues: {
+      userType: "",
+      cpfCnpj: "",
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: stepValidationSchemas[currentStep],
+    onSubmit: async (values) => {
+      if (currentStep < 2) {
+        setCurrentStep(currentStep + 1);
+      } else {
+        try {
+          const endpoint =
+            formik.values.userType === "clinic"
+              ? "http://localhost:8080/api/register-clinic"
+              : "http://localhost:8080/api/register-user";
 
-    setCpfCnpj(formatted);
-  };
+          const response = await axios.post(endpoint, values);
 
-  const handlePhoneChange = (e) => {
-    const value = e.target.value.replace(/\D/g, ''); 
-    const formatted = value
-      .replace(/^(\d{2})(\d)/, '($1) $2') 
-      .replace(/(\d{5})(\d)/, '$1-$2'); 
-    setPhone(formatted);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
+          if (response.status === 200) {
+            alert("Cadastro realizado com sucesso!");
+            navigate("/login");
+          }
+        } catch (error) {
+          console.error("Erro no cadastro:", error);
+          alert("Ocorreu um erro ao realizar o cadastro.");
+        }
+      }
+    },
+  });
 
   return (
     <Background>
       <RegisterContainer>
         <RegisterCard>
           <Logo src={logoImage} alt="Logo Meu PET" />
-          <Title>Cadastre-se</Title>
-          <Input
-            type="text"
-            placeholder="CPF ou CNPJ*"
-            value={cpfCnpj}
-            onChange={handleCpfCnpjChange}
-            maxLength="18"
-          />
-          <Input placeholder="Nome Completo*" />
-          <Input placeholder="Nome social*" />
-          <Input type="date" placeholder="Data de Nascimento*" />
-          <Input
-            type="email"
-            placeholder="E-mail*"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <Input
-            type="text"
-            placeholder="Telefone*"
-            value={phone}
-            onChange={handlePhoneChange}
-            maxLength="15" 
-          />
-          <InputContainer>
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Senha*"
-            />
-            <span onClick={() => setShowPassword((prev) => !prev)}>
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </span>
-          </InputContainer>
-          <InputContainer>
-            <Input
-              type={showConfirmPassword ? 'text' : 'password'}
-              placeholder="Confirme a senha*"
-            />
-            <span onClick={() => setShowConfirmPassword((prev) => !prev)}>
-              {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
-            </span>
-          </InputContainer>
-          <LinkText onClick={() => navigate('/login')}>Já tem uma conta? Login</LinkText>
-          <Button secondary onClick={() => navigate('/')}>Voltar</Button>
-          <Button>Cadastrar-se</Button>
+          <Title>
+            {currentStep === 0
+              ? "Como deseja se cadastrar?"
+              : formik.values.userType === "clinic"
+              ? "Cadastre aqui sua Clínica ou Serviços"
+              : "Cadastre-se"}
+          </Title>
+
+          {currentStep === 0 && (
+            <ButtonEscolhaContainer>
+              <ButtonEscolha
+                onClick={() => {
+                  formik.setFieldValue("userType", "clinic");
+                  setCurrentStep(currentStep + 1);
+                }}
+              >
+                Cadastrar Clínica
+              </ButtonEscolha>
+              <ButtonEscolha
+                secondary
+                onClick={() => {
+                  formik.setFieldValue("userType", "user");
+                  setCurrentStep(currentStep + 1);
+                }}
+              >
+                Cadastrar Usuário
+              </ButtonEscolha>
+            </ButtonEscolhaContainer>
+          )}
+
+          {currentStep === 1 && (
+            <form onSubmit={formik.handleSubmit}>
+              {formik.values.userType === "clinic" ? (
+                <>
+                  <InputContainer>
+                    <Label htmlFor="cpfCnpj">CPF ou CNPJ*</Label>
+                    <Input
+                      id="cpfCnpj"
+                      type="text"
+                      name="cpfCnpj"
+                      placeholder="Digite aqui"
+                      value={formik.values.cpfCnpj}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.cpfCnpj && formik.errors.cpfCnpj && (
+                      <ErrorText>{formik.errors.cpfCnpj}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="name">Nome Fantasia ou Pessoal*</Label>
+                    <Input
+                      id="name"
+                      type="text"
+                      name="name"
+                      placeholder="Digite aqui"
+                      value={formik.values.name}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.name && formik.errors.name && (
+                      <ErrorText>{formik.errors.name}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="email">E-mail*</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder="Digite aqui"
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email && (
+                      <ErrorText>{formik.errors.email}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="phone">Telefone*</Label>
+                    <Input
+                      id="phone"
+                      type="text"
+                      name="phone"
+                      placeholder="(XX) XXXXX-XXXX"
+                      value={formik.values.phone}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.phone && formik.errors.phone && (
+                      <ErrorText>{formik.errors.phone}</ErrorText>
+                    )}
+                  </InputContainer>
+                </>
+              ) : (
+                <>
+                  <InputContainer>
+                    <Label htmlFor="cpfCnpj">CPF*</Label>
+                    <Input
+                      id="cpfCnpj"
+                      type="text"
+                      name="cpfCnpj"
+                      placeholder="Digite aqui"
+                      value={formik.values.cpfCnpj}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.cpfCnpj && formik.errors.cpfCnpj && (
+                      <ErrorText>{formik.errors.cpfCnpj}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="name">Nome Completo*</Label>
+                    <Input
+                      id="name"
+                      type="text"
+                      name="name"
+                      placeholder="Digite aqui"
+                      value={formik.values.name}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.name && formik.errors.name && (
+                      <ErrorText>{formik.errors.name}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="socialName">Nome Social (opcional)</Label>
+                    <Input
+                      id="socialName"
+                      type="text"
+                      name="socialName"
+                      placeholder="Digite aqui"
+                      value={formik.values.socialName}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="birthDate">Data de Nascimento*</Label>
+                    <Input
+                      id="birthDate"
+                      type="date"
+                      name="birthDate"
+                      value={formik.values.birthDate || ""}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.birthDate && formik.errors.birthDate && (
+                      <ErrorText>{formik.errors.birthDate}</ErrorText>
+                    )}
+                  </InputContainer>
+                </>
+              )}
+
+              <ButtonContainer>
+                <Button
+                  secondary
+                  onClick={() => setCurrentStep(currentStep - 1)}
+                >
+                  Voltar
+                </Button>
+                <Button type="submit">Avançar</Button>
+              </ButtonContainer>
+            </form>
+          )}
+
+          {currentStep === 2 && (
+            <form onSubmit={formik.handleSubmit}>
+              {formik.values.userType === "clinic" ? (
+                <>
+                  <InputContainer>
+                    <Label htmlFor="password">Senha*</Label>
+                    <Input
+                      id="password"
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      placeholder="Digite sua senha"
+                      value={formik.values.password}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <span onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </span>
+                    {formik.touched.password && formik.errors.password && (
+                      <ErrorText>{formik.errors.password}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="confirmPassword">Confirme a senha*</Label>
+                    <Input
+                      id="confirmPassword"
+                      type={showConfirmPassword ? "text" : "password"}
+                      name="confirmPassword"
+                      placeholder="Digite novamente"
+                      value={formik.values.confirmPassword}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <span
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                    >
+                      {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                    </span>
+                    {formik.touched.confirmPassword &&
+                      formik.errors.confirmPassword && (
+                        <ErrorText>{formik.errors.confirmPassword}</ErrorText>
+                      )}
+                  </InputContainer>
+                </>
+              ) : (
+                <>
+                  <InputContainer>
+                    <Label htmlFor="email">E-mail*</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder="Digite aqui"
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email && (
+                      <ErrorText>{formik.errors.email}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="phone">Telefone*</Label>
+                    <Input
+                      id="phone"
+                      type="text"
+                      name="phone"
+                      placeholder="(XX) XXXXX-XXXX"
+                      value={formik.values.phone}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.phone && formik.errors.phone && (
+                      <ErrorText>{formik.errors.phone}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="password">Senha*</Label>
+                    <Input
+                      id="password"
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      placeholder="Digite sua senha"
+                      value={formik.values.password}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <span onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </span>
+                    {formik.touched.password && formik.errors.password && (
+                      <ErrorText>{formik.errors.password}</ErrorText>
+                    )}
+                  </InputContainer>
+
+                  <InputContainer>
+                    <Label htmlFor="confirmPassword">Confirme a senha*</Label>
+                    <Input
+                      id="confirmPassword"
+                      type={showConfirmPassword ? "text" : "password"}
+                      name="confirmPassword"
+                      placeholder="Digite novamente"
+                      value={formik.values.confirmPassword}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <span
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                    >
+                      {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                    </span>
+                    {formik.touched.confirmPassword &&
+                      formik.errors.confirmPassword && (
+                        <ErrorText>{formik.errors.confirmPassword}</ErrorText>
+                      )}
+                  </InputContainer>
+                </>
+              )}
+
+              <ButtonContainer>
+                <Button
+                  secondary
+                  onClick={() => setCurrentStep(currentStep - 1)}
+                >
+                  Voltar
+                </Button>
+                <Button type="submit">Cadastrar</Button>
+              </ButtonContainer>
+            </form>
+          )}
         </RegisterCard>
       </RegisterContainer>
     </Background>
