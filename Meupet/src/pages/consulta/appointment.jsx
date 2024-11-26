47c449e7ca7a0c16d852e44e64ac1de57e256f84
Gabi Melo <gabriellesilvamelo622@gmail.com>
2024-11-26 19:49:31 +0000

    tela finalizada

--- Meupet/src/pages/consulta/appointment.jsx
+++ Meupet/src/pages/consulta/appointment.jsx
@@ -11,8 +11,6 @@ import { MdLocalPhone } from "react-icons/md";
 import { HeaderSection, MainForm, InputGrid, ButtonGroup,SlideItem } from "./appointmentstyle";
 import "swiper/css"
 
-
-
 const Appointment = () => {
  
   const data =[
@@ -33,16 +31,16 @@ const Appointment = () => {
           <div className="text">
             <h1>Centro Médico Veterinário</h1>
             <h2>Sua Clínica Veterinária: Saúde e Cuidado para o seu Pet</h2>
-            <div>
-              <PiStethoscopeDuotone />
+            <div className="divInformacoes">
+              <PiStethoscopeDuotone className="headerIcones"/>
               <p><strong> +4 Serviços Inclusos: </strong> Consultas,Exames e Cirurgias</p>
             </div> 
-            <div>
-              <BsClipboardCheck />
+            <div className="divInformacoes">
+              <BsClipboardCheck className="headerIcones"/>
               <p><strong>Atendimento: Das 8h às 18h,</strong>todos os dias</p>
             </div> 
-            <div>
-              <MdLocalPhone />
+            <div className="divInformacoes">
+              <MdLocalPhone className="headerIcones"/>
               <p>Em caso de urgências, ligue: <strong>(81) 3440-0443</strong></p>
             </div> 
           </div>
@@ -51,7 +49,6 @@ const Appointment = () => {
       </HeaderSection>
 
       <Swiper
-      
             spaceBetween={50}
             slidesPerView={1}
             pagination={{clickable:true}}
@@ -73,46 +70,49 @@ const Appointment = () => {
       </Swiper>
 
       <MainForm>
-        <div>
+        <div className="tituloFormulario">
            <h1>Serviços de Agendamento</h1>
            <h2>Preencha Rapidamente as informações de seu Pet e <strong> Agende com Facilidade:</strong></h2>
         </div>
         <InputGrid>
           <div>
             <h1>Informações do Animal:</h1>
-            <div>
-              <div>
+            <div className="checkboxArea">
+            
                 <h1>Tipo do animal:</h1>
                 <input type="checkbox" id="cachorro" /> 
                 <label for= "cachorro">Cachorro </label>  
                 <input type="checkbox" id="gato" /> 
                 <label for= "gato">Gato </label>  
-              </div>
-              <div>
-                <h1>Gênero do Pet::</h1>
+      
+                <h1 className="segundoTitulo">Gênero do Pet:</h1>
                 <input type="checkbox" id="femea" /> 
                 <label for= "femea">Fêmea </label>  
                 <input type="checkbox" id="macho" /> 
                 <label for= "macho">Macho</label>  
-              </div>
+           
+            </div>
+            
+          </div>
+          <div className="divContain">
+            <div className="divForm">
+              <label>Nome do Pet</label>
+              <input type="text" placeholder="Insira o nome do seu Pet" />
+            </div>
+            <div className="divForm">
+              <label>Idade</label>
+              <input type="email" placeholder="Exemplo: 3 anos" />
+            </div>
+            <div className="divForm">
+              <label>Histórico médico relevante (se houver).</label>
+              <input type="email" placeholder="Insira aqui as informações adicionais sobre o seu Pet" />
             </div>
           </div>
-          <div>
-            <label>Nome do Pet</label>
-            <input type="text" placeholder="Insira o nome do seu Pet" />
-          </div>
-          <div>
-            <label>Idade</label>
-            <input type="email" placeholder="Exemplo: 3 anos" />
-          </div>
-          <div>
-            <label>Histórico médico relevante (se houver).</label>
-            <input type="email" placeholder="Insira aqui as informações adicionais sobre o seu Pet" />
-          </div>
+
           <div>
             <h1>Detalhes do serviço: </h1>
             </div>
-            <div>
+            <div className="checkboxArea">
                 <h1>Tipo do animal:</h1>
                 <input type="checkbox" id="consulta" /> 
                 <label for= "consulta">Consulta </label>  
@@ -121,18 +121,20 @@ const Appointment = () => {
                 <input type="checkbox" id="vacinas" /> 
                 <label for= "vacinas">Vacinas</label>  
               </div>
-          <div>
-            <label>Data do Serviço</label>
-            <input type="number" placeholder="Data" />
-          </div>
-          <div>
-            <label>Horários</label>
-            <textarea placeholder="Horários"></textarea>
-          </div>
+          <div className="divContain">
+            <div className="divForm">
+              <label>Data do Serviço</label>
+              <input type="number" placeholder="Data" />
+            </div>
+            <div className="divForm">
+              <label>Horários</label>
+              <textarea placeholder="Horários"></textarea>
+            </div>
+          </div> 
         </InputGrid>
 
         <ButtonGroup>
-          <button className="primary">Efetuar Marcação</button>
+          <button >Efetuar Marcação</button>
         </ButtonGroup>
       </MainForm>
 
--- Meupet/src/pages/consulta/appointmentstyle.js
+++ Meupet/src/pages/consulta/appointmentstyle.js
@@ -14,126 +14,172 @@ export const CarrosselImage = styled.div`
 `;
 
 export const HeaderSection = styled.div`
-background-color: #00507b;
-color: white;
-text-align: center;
-padding: 20px;
+  width: 80%;
+  background-color: #00507b;
+  color: white;
+  border-radius:20px;
+  padding: 20px;
+  margin:24px auto;
 
-div{
-  margin: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 24px;
-
-  .text{
+  div{
     display: flex;
-    flex-direction: column;
-    gap: 6px;
-    margin: 0;
+    flex-direction: row;
+    
+    .text{
+      display: flex;
+      flex-direction: column;
+      gap: 6px;
+      margin: 0;
+    }
+
   }
 
-}
-
-img {
-  min-width: 120px;
+  img {
+    width: 300px;
+    heigth: 300px;
   }
 
   h1 {
-    font-size: 24px;
+    font-size: 70px;
   }
   
   h2 {
+    width: 80%;
+    background-color: #8BC500;
+    border-radius: 20px;
+    font-family: "Poppins";
     font-size: 18px;
-    color: #82c83a;
+    color: #FFFFF;
+    padding: 6px 14px;
   }
 
   p {
+    font-family: "Poppins";
     font-size: 14px;
+  }
+
+  .divInformacoes{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+    .headerIcones {
+      width: 30px; 
+      height: 30px; 
+      margin: 12px;
+    }
   }
 `;
 
-
 export const MainForm = styled.div`
-  max-width: 1000px;
+  width: 80%;
   margin: 20px auto;
   padding: 20px;
   background: #f8f8f8;
   border-radius: 10px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  h2{
+
+  .tituloFormulario{
+    display:flex;
+    flex-direction: row;
+    align-items: center;
     margin-bottom: 16px;
-  } 
+
+    h1{
+      background-color: #2A4F6A;  
+      color: #FFFFFF;
+      font-weight: normal;
+      padding: 4px 8px;
+      border-radius: 8px;
+    }
+
+    h2{
+      font-family: "Poppins";
+      font-weight: normal;
+      font-size: 16px;
+      padding: 4px 8px;
+      border-radius: 8px;
+    }
+  }
 `;
 
 export const InputGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  width:100%;
+  display: flex;
+  flex-direction: column;
   gap: 15px;
   margin-bottom: 20px;
 
-  label {
-    display: block;
-    font-size: 14px;
-    margin-bottom: 5px;
-  }
+      h1{
+      font-family: "Poppins";
+      font-size: 16px;
+    }
 
-  input,
-  textarea {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #d5d5d5;
-    border-radius: 5px;
-    font-size: 14px;
-  }
-
-  textarea {
-    resize: none;
-    height: 80px;
-  }
-
-  .divInputRadio{
+  .checkboxArea{
+    font-family: "Poppins";
     display: flex;
-	  flex-direction: column; 
-    gap: 8px;
+    flex-direction: row;
+    align-items: center;
+
+    input{
+    width:50px;
+    heigth:50px;
+    }
+
+    .segundoTitulo{
+    margin:0 0 0 80px;
+    }
+
   }
 
-  .inputRadio {
-	display: flex;
-	flex-direction: row; 
-  label{
-    width: 20px;
+  .divContain{
+  display:flex;
+
+    .divForm{
+      margin: 0 16px 0 0;
+
+      input, textarea{
+        padding:16px;
+        width: 428px;
+        height: 50px;
+        border: 1px solid #2A4F6A;
+        border-radius: 12px;
+      }
+      
+      label{
+      display: block;
+      font-size:20px;
+      }
   }
   }
+}
 `;
 
 
 export const ButtonGroup = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 15px;
+    display: flex;
+    justify-content: center;
+    button {
+      font-size: 20px;
+      background-color: #2A4F6A;
+      border:1px solid #094DB6;
+      border-radius: 12px;
+      color: #FFFFFF;
+      padding: 8px 24px;
+      cursor: pointer;
+    }
 
-  button {
-    padding: 10px 20px;
-    font-size: 16px;
-    font-weight: bold;
-    border-radius: 25px;
-    cursor: pointer;
-  }
-
-  .primary {
-    background-color: #82c83a;
-    color: white;
-  }
-
-   .secondary {
-     background-color: #82c83a;
-     color: white;
-   }
+    button:hover{
+      background-color: #516675;
+    }
 `;
 
 export const SlideItem = styled.img`
-    width: 100%;
-    height:100%;
-    object-fit:cover;
-`;
+  width: 80%;
+  height: 100%;
+  border-radius: 20px;
+  display: block;
+  margin: 0 auto;  
+
+  `;
+
