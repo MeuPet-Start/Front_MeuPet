0c22e229a64df53c1ccabb477ed0123735dcda68
Natan Lima <nfsl@cin.ufpe.br>
2024-11-29 19:04:01 -0300

    Header Responsivo

--- Meupet/src/components/header/header.jsx
+++ Meupet/src/components/header/header.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logoImage from "../../assets/logo.png";
-import userImage from "../../assets/logo.png"; 
-import clinicImage from "../../assets/logo.png"; 
+import userImage from "../../assets/logo.png";
+import clinicImage from "../../assets/logo.png";
 import coin from "../../assets/capiba.png";
 
 import {
@@ -20,17 +20,38 @@ import {
   ClinicContainer,
   ProfileImage,
   UserName,
+  MenuIcon,
+  MobileMenu,
+  MobileNavLink,
+  CloseButton,
 } from "./headerStyle";
 
-import {useUserType} from "../../hooks/useUserType";
-import {useUserData} from "../../hooks/useUserData";
-
+import { useUserType } from "../../hooks/useUserType";
+import { useUserData } from "../../hooks/useUserData";
 
 const Header = () => {
   const navigate = useNavigate();
-  const {userType, userEmail} = useUserType();
-  const {userName, userPoints} = useUserData(userEmail);
-  
+  const { userType, userEmail } = useUserType();
+  const { userName, userPoints } = useUserData(userEmail);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <Nav>
@@ -38,46 +59,113 @@ const Header = () => {
         <Logo src={logoImage} alt="Logo Meu PET" />
         <LogoText>Meu PET</LogoText>
       </LogoContainer>
-      <Links>
-        <NavLink onClick={() => navigate("/")}>Home</NavLink>
-        <NavLink
-          as="a"
-          href="https://adotapet.recife.pe.gov.br/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          AdotaPet
-        </NavLink>
-        <NavLink onClick={() => navigate("/servicos")}>Serviços</NavLink>
-        {userType === "user"  && (
-          <NavLink onClick={() => navigate("/consultas")}>Consultas</NavLink>
-        )}
-        {userType === "clinic"  && (
-          <NavLink onClick={() => navigate("/consultas")}>Consultas</NavLink>
-        )}
-      </Links>
-      {userType ? (
-        <UserSection>
+
+      {!isMobile && (
+        <Links>
+          <NavLink onClick={() => navigate("/")}>Home</NavLink>
+          <NavLink
+            as="a"
+            href="https://adotapet.recife.pe.gov.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AdotaPet
+          </NavLink>
+          <NavLink onClick={() => navigate("/servicos")}>Serviços</NavLink>
           {userType === "user" && (
+            <NavLink onClick={() => navigate("/consultas")}>Consultas</NavLink>
+          )}
+          {userType === "clinic" && (
+            <NavLink onClick={() => navigate("/consultas")}>Consultas</NavLink>
+          )}
+        </Links>
+      )}
+
+      {isMobile && <MenuIcon onClick={toggleMenu} />}
+
+      {isMenuOpen && (
+        <MobileMenu>
+          <CloseButton onClick={toggleMenu}>X</CloseButton>
+          <MobileNavLink onClick={() => navigate("/")}>Home</MobileNavLink>
+          <MobileNavLink
+            as="a"
+            href="https://adotapet.recife.pe.gov.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AdotaPet
+          </MobileNavLink>
+          <MobileNavLink onClick={() => navigate("/servicos")}>
+            Serviços
+          </MobileNavLink>
+          {userType === "user" && (
+            <MobileNavLink onClick={() => navigate("/consultas")}>
+              Consultas
+            </MobileNavLink>
+          )}
+          {userType === "clinic" && (
+            <MobileNavLink onClick={() => navigate("/consultas")}>
+              Consultas
+            </MobileNavLink>
+          )}
+
+          {userType ? (
             <>
+              {userType === "user" && (
+                <UserContainer>
+                  <ProfileImage src={userImage} alt="Usuário" />
+                  <UserName onClick={() => navigate("/perfil-user")}>
+                    {userName}
+                  </UserName>
+                </UserContainer>
+              )}
+              {userType === "clinic" && (
+                <ClinicContainer>
+                  <ProfileImage src={clinicImage} alt="Clínica" />
+                  <UserName onClick={() => navigate("/perfil-clinica")}>
+                    {userName}
+                  </UserName>
+                </ClinicContainer>
+              )}
               <CoinsContainer>
                 <CoinIcon src={coin} alt="Moedas" />
                 <CoinCount>{userPoints || 0}</CoinCount>
               </CoinsContainer>
-              <UserContainer>
-                <ProfileImage src={userImage} alt="Usuário" />
-                <UserName onClick={() => navigate("/perfil-user")}>{userName}</UserName>
-              </UserContainer>
             </>
+          ) : (
+            <MobileNavLink onClick={() => navigate("/login")}>
+              Entrar
+            </MobileNavLink>
+          )}
+        </MobileMenu>
+      )}
+
+      {!isMobile && userType && (
+        <UserSection>
+          {userType === "user" && (
+            <UserContainer>
+              <ProfileImage src={userImage} alt="Usuário" />
+              <UserName onClick={() => navigate("/perfil-user")}>
+                {userName}
+              </UserName>
+            </UserContainer>
           )}
           {userType === "clinic" && (
             <ClinicContainer>
               <ProfileImage src={clinicImage} alt="Clínica" />
-              <UserName onClick={() => navigate("/perfil-clinica")}>{userName}</UserName>
+              <UserName onClick={() => navigate("/perfil-clinica")}>
+                {userName}
+              </UserName>
             </ClinicContainer>
           )}
+          <CoinsContainer>
+            <CoinIcon src={coin} alt="Moedas" />
+            <CoinCount>{userPoints || 0}</CoinCount>
+          </CoinsContainer>
         </UserSection>
-      ) : (
+      )}
+
+      {!isMobile && !userType && (
         <NavLink onClick={() => navigate("/login")}>Entrar</NavLink>
       )}
     </Nav>
